db0269989567d3d4f2d6a5094b879dfc3f29fcb5
EC2 Default User <ec2-user@ip-172-31-15-98.ec2.internal>
2021-12-09 12:34:28 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,60 @@
+class BooksController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
+  
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @books = Book.all
+    @user = current_user
+    if @book.save
+      redirect_to book_path(@book.id)
+      flash[:notice] = "You have created book successfully."
+    else
+      render :index
+    end
+  end
+  
+  def show
+    @book = Book.find(params[:id])
+    @book_new = Book.new
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[:notice] = "You have created book successfully."
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
+  devise_for :users
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
 end
